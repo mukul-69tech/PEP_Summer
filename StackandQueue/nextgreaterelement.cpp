aa10b4e5e492103e163e284d39a9c5f56de08366
mukul <mukulchauhan0029@gmail.com>
2026-07-14 16:16:56 +0530

    initial commit

---
+++ StackandQueue/nextgreaterelement.cpp
@@ -0,0 +1,29 @@
+//https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> nge(n);
+        
+        for(int i = n-1; i>=0;i--){
+            while(!st.empty() && st.top() <= arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                nge[i] = -1;
+            }
+            else {
+                nge[i] = st.top();
+            }
+            st.push(arr[i]);
+        }
+        return nge;
+        
+    }
+};
