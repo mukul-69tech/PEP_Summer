ad7d8d570e7de223f0635988f82701cda9708b8f
mukul <mukulchauhan0029@gmail.com>
2026-07-20 14:41:15 +0530

    initial commit

---
+++ Trees/ConvertSortedArraytoBinarySearchTree.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+
+#include<iostream>
+using namespace std; 
+
+class Solution {
+private:
+    TreeNode* helper(const std::vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = helper(nums, left, mid - 1);
+        root->right = helper(nums, mid + 1, right);
+
+        return root;
+    }
+
+public:
+    TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+};
