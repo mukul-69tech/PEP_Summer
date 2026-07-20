4e4feea6165114d8c24eb96c0ba8451f122dd1a8
mukul <mukulchauhan0029@gmail.com>
2026-07-20 15:16:08 +0530

    initial commit

---
+++ Trees/KthSmallestElementinaBST.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/\
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& ans){
+        if(root==NULL) return;
+
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> ans;
+        inorder(root, ans);
+        return ans[k-1];
+    }
+};
